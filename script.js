70961ec36fedb740b5b490df6c7b72154cf39de9
Kama <kamasimbeye89@gmail.com>
2024-03-09 15:58:01 +0200

    reaaranged code to avoid confusion

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+let currentValue ='';
+let theOperator ='';
+let previousValue ='';
+
+document.addEventListener ('DOMContentLoaded', function(){
+
+
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.number');
 const operators=document.querySelectorAll('.operator');
@@ -7,71 +15,57 @@ const decimal =document.querySelector('.decimalPoint');
 const previous = document.querySelector('.previous');
 const current = document.querySelector('.current');
 
-function add(a,b){
-    return a+b;
-}
 
-function subtract(a,b){
-    return a-b;
-}
-function multiply(a,b){
-    return a*b;
-}
-function divide(a,b){
-    return a/b;
-}
-
-let currentValue ='';
-let theOperator ='';
-let previousValue ='';
-let isClicked = true;
-
-/*function operator (firstNum,secondNum,operator){
-    if ( operator ==='+'){
-        return add(getNums);
-    }
-    else if (operator=== '-'){ 
-        return subtract(firstNum,secondNum);
-    }
-    else if (operator=== 'x'){
-        return multiply(firstNum,secondNum);
-    }
-    else if (operator==='/'){
-        return divide(firstNum,secondNum);
-    }
-}
-*/
-
-
-
-
-//function populateDisplay(){
-    numbers.forEach(number => {
+ numbers.forEach(number => {
     number.addEventListener('click', (e)=>{
         handleNum(e.target.textContent);
-        current.textContent = currentValue,'';
+
+     currentValue = current.textContent += parseInt(number.textContent,10);
+     currentValue= Number(currentValue);
+
           })
         })
 
-    operators.forEach(operator => {
-        operator.addEventListener('click',(op)=>{
-            operation(op.target.textContent)
+
+ operators.forEach(operator => {
+    operator.addEventListener('click',(e)=>{
+       operation(e.target.textContent)
             previous.textContent = previousValue + " " +theOperator;
             current.textContent = currentValue;
         })
-    })
+    })  
+
+    equal.addEventListener('click', ()=> {
+        performOperation();
+        previous.textContent = performOperation();
+        })
+
+    
+            clearButton.addEventListener('click', ()=>{
+                previousValue = '';
+                currentValue = '';
+                theOperator= '';
+                previous.textContent = previousValue;
+                current.textContent = currentValue;
+            })
+        
+
+})
 
 
+function add(previousValue,currentValue){
 
+    return previousValue + currentValue;
+}
 
-function clearDisplay(){
-    clearButton.addEventListener('click', ()=>{
-        previousValue = '';
-        currentValue = '';
-        theOperator= '';
-        previous.textContent = previousValue;
-        current.textContent = currentValue;
-    })
+function subtract(previousValue,currentValue){
+    return previousValue-currentValue;
+}
+function multiply(previousValue,currentValue){
+    return previousValue*currentValue;
+}
+function divide(a,b){
+    return a/b;
 }
 
 
@@ -84,5 +78,29 @@ function operation (op){
     previousValue = currentValue;
     currentValue='';
 }
-clearDisplay();
+
+
+function performOperation (){
+    //currentValue = (currentValue);
+    //previousValue =  (previousValue);
+    
+
+   if ( theOperator === '+'){ 
+    return add(previousValue,currentValue)
+            }
+
+    else if (theOperator === '-'){ 
+        return subtract(currentValue,previousValue);
+    }
+    else if (theOperator === 'x'){
+        return multiply(currentValue,previousValue);
+    }
+    else if (theOperator ==='/'){
+        return divide(currentValue,previousValue);
+    }
+
+    //console.log (result);
+}
+
+;
 //populateDisplay();
